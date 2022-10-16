aea12314566289069f630cf2f1450ee281c714e8
MikeGer92 <MikeGer@yandex.ru>
2022-10-16 09:18:36 +0300

    added Layouts

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Header />
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Header from '@/components/app/Header.vue'
+export default {
+  name: 'MainLayout',
+  components: { Header }
+}
+</script>
+
+<style lang="scss">
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/index.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: Home
+    meta: {layout: 'main'},
+    component: () => import('@/views/index.vue'),
   },
 ]
 
